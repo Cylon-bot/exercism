f2b9caa9726b21f8d06440739c93250641a16d83
Thibault delrieu <thibault.delrieu.pro@gmail.com>
2023-12-09 17:59:18 +0100

    finished clock exercise

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,12 +1,36 @@
-pub struct Clock;
-
+use std::fmt;
+#[derive(Debug)]
+pub struct Clock {
+    hours: u32,
+    minutes: u32,
+}
+pub fn calculate_hour(hours: u32, prev_minutes: u32, minutes: i32) -> u32 {
+    (hours as i32 + ((minutes + prev_minutes as i32) as f64 / 60.0).floor() as i32).rem_euclid(24)
+        as u32
+}
 impl Clock {
-    
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock {
+            hours: calculate_hour(hours.rem_euclid(24) as u32, 0, minutes),
+            minutes: minutes.rem_euclid(60) as u32,
+        }
     }
+    pub fn add_minutes(self, minutes: i32) -> Self {
+        Clock {
+            hours: calculate_hour(self.hours, self.minutes, minutes),
+            minutes: (self.minutes as i32 + minutes).rem_euclid(60) as u32,
+        }
+    }
+}
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
+
+impl std::cmp::PartialEq for Clock {
+    fn eq(&self, other: &Clock) -> bool {
+        self.hours == other.hours && self.minutes == other.minutes
     }
 }
